cd44fb9af195564456ada45654f41bfaeabd0136
Sofia <117852305+Sofia777333@users.noreply.github.com>
2022-11-10 12:23:10 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task
+{
+
+
+
+    class Example
+    {
+        static void Main()
+        {
+            int[] fibonacciNumbers = GenerateFibonacciNumbers(20);
+            Print(fibonacciNumbers);
+        }
+        private static void Print<T>(T[] array)
+        {
+            foreach (T item in array)
+                Console.WriteLine(item);
+        }
+        static int[] GenerateFibonacciNumbers(int length)
+        {
+            int[] fibonacciNumbers = new int[length];
+            fibonacciNumbers[0] = fibonacciNumbers[1] = 1;
+            {
+                for (int i = 2; i < length; i++)
+                    fibonacciNumbers[i]=fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+                return fibonacciNumbers;
+            }
+        }
+
+
+    }
+}
